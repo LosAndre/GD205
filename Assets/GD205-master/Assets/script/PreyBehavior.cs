c2783aa942000206bb740a89140515f40648fd2d
Peepee PooPoo <xdddddd@pepe.com>
2020-03-05 20:20:45 -0500

    Hunter Repository

---
+++ Assets/GD205-master/Assets/script/PreyBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PreyBehavior : MonoBehaviour
+{
+    Rigidbody rb;
+    public Transform predator;
+    public float terrorForce = 10f;
+    public string predatorTag, preyTag;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        Vector3 predatorDirection = Vector3.Normalize(predator.position - transform.position);
+
+        rb.AddForce(-predatorDirection * terrorForce);
+
+        
+    }
+    void OnCollisionEnter(Collision myCollision)
+    {
+        if (myCollision.gameObject.CompareTag(predatorTag))
+        {
+            Destroy(myCollision.gameObject);
+        }
+        else if (myCollision.gameObject.CompareTag(preyTag))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/GD205-master/Assets/script/predatorbehavior.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class predatorbehavior : MonoBehaviour
+{
+    Rigidbody rb;
+    public Transform prey;
+    public float hungerForce = 10f;
+    public string predatorTag, preyTag;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        Vector3 preyDirection = Vector3.Normalize(prey.position - transform.position);
+
+        rb.AddForce(preyDirection * hungerForce);
+    }
+    void OnCollisionEnter(Collision myCollision)
+    {
+        if (myCollision.gameObject.CompareTag(preyTag))
+        {
+            Destroy(myCollision.gameObject);
+        }
+        else if (myCollision.gameObject.CompareTag(predatorTag))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
